4588a790a50aa651bb5f1f98bea153d55c6e3fba
Javier Ferrer Sánchez-Barcaiztegui <javierferrersb@gmail.com>
2025-12-30 18:06:28 +0100

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import { LocaleProvider } from "@/i18n/LocaleContext";
+import { Footer } from "@/components/Footer";
 import { Locale } from "@/i18n/config";
 import "./globals.css";
 
@@ -23,12 +24,13 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <body>
+      <body className="flex flex-col min-h-screen">
         <NextIntlClientProvider messages={messages}>
           <LocaleProvider initialLocale={locale}>
-            <main className="main-content min-h-screen bg-orange-200">
+            <main className="main-content flex-1 min-h-screen bg-orange-200">
               {children}
             </main>
+            <Footer />
           </LocaleProvider>
         </NextIntlClientProvider>
       </body>
